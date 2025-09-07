8a1cd235a52d95ab3b0d4d99a87dd4299816a7d3
Geetha936 <Korukonda076@gmail.com>
2025-09-07 15:30:28 +0530

    initial push

---
+++ spring/demo/src/test/java/com/Smart/demo/SmartTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
